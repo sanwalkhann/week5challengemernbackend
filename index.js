3755c3979b82acd13454d8072af41280527aa99b
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-28 01:45:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-// server.js
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const passport = require("./config/passport"); // Assuming you have a passport configuration
+const passport = require("./config/passport");
 const cors = require("cors");
 const { connectDB } = require("./config/db");
 const categoryRoutes = require("./Model/categories");
-
-// Importing passport configurations and other necessary modules
 require("./config/passport");
 require("./auth");
 require("dotenv").config();
 
 const app = express();
 
-// Enable CORS for the specified origin
 app.use(
   cors({
-    origin: "https://hackathonweekfivesanwal.vercel.app",
+    origin: "http://localhost:3500",
     credentials: true,
   })
 );
@@ -28,46 +23,14 @@ app.options("*", cors());
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-// Connect to MongoDB
 connectDB();
 
-// Routes
 app.use("/auth", require("./Routes/auth"));
 app.use("/tasks", require("./Routes/routes"));
 app.use("/categories", categoryRoutes);
 
-// Route for checking authentication status
-app.get("/auth/check", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.json({ isAuthenticated: true });
-  } else {
-    res.json({ isAuthenticated: false });
-  }
-});
+// Your existing route for checking authentication
 
-// Route for handling Google authentication
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/" }),
-  (req, res) => {
-    // Redirect after successful authentication
-    res.redirect("/dashboard");
-  },
-  (err, req, res, next) => {
-    // Handle authentication failure
-    if (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    }
-  }
-);
-
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
