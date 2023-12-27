e32c242fa14f6a07a53c5993b10e3a08747dc4f3
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-28 02:06:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(
-  cors()
-);
-
-
-// app.options("*", cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+ 
+ app.use(cors());
 
 app.use(bodyParser.json());
 app.use(passport.initialize());
