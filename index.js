00e6f9277b0a8e0f32677b1f895922eb63b7bddb
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-28 11:12:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors())
 // to handler cors
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://vercel.com/sanwals-projects/hackathonweekfivesanwal');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
