acbf7e0a64306de9f117c876a3b9ed4221ab5e5c
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-28 10:59:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors())
 // to handler cors
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
