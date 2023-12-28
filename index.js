7ae5b3b01b3a1bc7bcf737d4f40982d6e5089e90
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2023-12-28 10:46:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,64 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("./config/passport");
+const cors = require("cors");
 const { connectDB } = require("./config/db");
 const categoryRoutes = require("./Model/categories");
 require("./config/passport");
-require("./auth");
-require("dotenv").config();
+require('./auth')
+// Load environment variables from a .env file if available
+require('dotenv').config();
 
 const app = express();
+
+app.options('*',cors())
+// to handler cors
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
 connectDB();
 
+// Routes
 app.use("/auth", require("./Routes/auth"));
 app.use("/tasks", require("./Routes/routes"));
-app.use("/categories", categoryRoutes);
+app.use('/categories', categoryRoutes);
 
-app.get("/auth/check", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.json({ isAuthenticated: true });
-  } else {
-    res.json({ isAuthenticated: false });
-  }
+// Route to check authentication
+app.get('/auth/check', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.json({ isAuthenticated: true });
+    } else {
+        res.json({ isAuthenticated: false });
+    }
 });
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+// Google OAuth
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { failureRedirect: "/" }),
+app.get('/auth/google',
+  passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect("/dashboard");
+    res.redirect('/dashboard');
   },
   (err, req, res, next) => {
     if (err) {
       console.error(err);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send('Internal Server Error');
     }
   }
 );
 
+
+
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 );
